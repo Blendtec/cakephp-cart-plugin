2110611c77889c7f28020e37fd04285dc0e2a275
Guy Warner <gwarner@blendtec.com>
2014-05-22 08:44:36 -0600

    Allow add to cart to bump up qty
    Summary: Refs T1507
    
    Test Plan:
    * you can't use your script for this
    * git pull blendtec's origin develop
    * cd Plugin/Cart
    * git checkout feature/orders
    * arc patch D### --nobranch
    * go to a product page, add  to cart, go back to product page add to cart again, should be 2 of them now
    * if it all works accept said diff
    * then cd ../..
    * git submodule update
    * git status you should be back to normal, maybe
    
    Reviewers: #everyone, jslemboski
    
    Reviewed By: #everyone, jslemboski
    
    Subscribers: jslemboski, lwhite
    
    Maniphest Tasks: T1507
    
    Differential Revision: https://dev.blendtec.com/D967

--- Controller/Component/CartSessionComponent.php
+++ Controller/Component/CartSessionComponent.php
@@ -57,10 +57,7 @@ class CartSessionComponent extends Component {
 			$this->write('CartsItem', $cart);
 			return $item;
 		} else {
-			if (!empty($item['increment'])) {
-				$item['quantity'] += $this->read('CartsItem.' . $arrayKey . '.quantity');
-				unset($item['increment']);
-			}
+			$item['quantity'] += $this->read('CartsItem.' . $arrayKey . '.quantity');
 			$this->write('CartsItem.' . $arrayKey, $item);
 			return $item;
 		}
